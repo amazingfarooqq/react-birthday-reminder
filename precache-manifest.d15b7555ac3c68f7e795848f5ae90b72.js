e8b31e1ac7e089f72b1240014ca65375a4fe2aff
Farooq Dad <farooqdaadkhan@gmail.com>
2022-03-11 03:55:34 +0500

    Updates

---
+++ precache-manifest.d15b7555ac3c68f7e795848f5ae90b72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9acb672e62e2a4c9dead6e9b567add2e",
+    "url": "/react-birthday-reminder/index.html"
+  },
+  {
+    "revision": "eb5c3b97474f416907f6",
+    "url": "/react-birthday-reminder/static/css/main.aad07e3e.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/react-birthday-reminder/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-birthday-reminder/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb5c3b97474f416907f6",
+    "url": "/react-birthday-reminder/static/js/main.a976039d.chunk.js"
+  },
+  {
+    "revision": "ca18d9339a309e58c265",
+    "url": "/react-birthday-reminder/static/js/runtime-main.4fe2f5be.js"
+  }
+]);
